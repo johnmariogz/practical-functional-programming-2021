0b1237e6adf54602558a095e0114e573e1f2408a
John Gutierrez-Zapata <266646+johnmariogz@users.noreply.github.com>
2021-10-05 19:02:18 +0200

    Make first example simpler (#3)

--- src/main/scala/session2/part_3_pattern_matching/Part1_PatternMatchingBasic.scala
+++ src/main/scala/session2/part_3_pattern_matching/Part1_PatternMatchingBasic.scala
@@ -2,20 +2,16 @@ package berlin.code.functional
 package session2.part_3_pattern_matching
 
 object Part1_PatternMatchingBasic extends App {
-  trait Foo
-  class Bar  extends Foo
-  class Bizz extends Foo
-
   // TODO Print "We have a Bar" or "A Bizz just came in"
-  def giveBackGreeting(foo: Foo): String = {
+  def giveBackGreeting(name: String): String = {
     ???
   }
 
-  val bar: Bar            = new Bar
+  val bar                 = "Bar"
   val greetingBar: String = giveBackGreeting(bar)
   println(s"The greeting for Bar is: $greetingBar")
 
-  val bizz: Bizz           = new Bizz
+  val bizz                 = "Bizz"
   val greetingBizz: String = giveBackGreeting(bizz)
   println(s"The greeting for Bizz is: $greetingBizz")
 }
