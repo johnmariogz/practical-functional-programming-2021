576295a17247a49d6809343abf29ae2b622452ca
John Gutierrez-Zapata <266646+johnmariogz@users.noreply.github.com>
2021-10-11 19:37:08 +0200

    Add extra tests to the homework (#5)

--- src/main/scala/exercises/session2/Recursion.scala
+++ src/main/scala/exercises/session2/Recursion.scala
@@ -5,4 +5,9 @@ object Recursion {
   // E.g. 5! = 120 => factorialLength(5) = 3
   // Parameter n can be [1, 10.000]
   def factorialLength(n: Int): Int = ???
+
+  // Function that returns the maximum continuous subsequence of numbers
+  // The function should go over the list only once.
+  // E.g. [1, 2, 3] => 6
+  def maxSubsequence(list: List[Int]): Int = ???
 }
