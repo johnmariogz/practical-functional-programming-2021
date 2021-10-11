3229329b87eeac2d4b8f3a6ca12a38ba28873975
John Gutierrez <johnmario91@gmail.com>
2021-10-11 19:35:49 +0200

    Add extra tests to the homework

--- src/test/scala/exercises/session2/RecursionTest.scala
+++ src/test/scala/exercises/session2/RecursionTest.scala
@@ -12,5 +12,35 @@ class RecursionTest extends AnyFlatSpec with Matchers {
     Recursion.factorialLength(6) shouldBe 3
   }
 
-  // TODO: Write extra tests. What happens with 50! and 100!
+  "#factorialLength" should "return 158 for 100!" in {
+    Recursion.factorialLength(100) shouldBe 158
+  }
+
+  "#factorialLength" should "return 2568 for 1000!" in {
+    Recursion.factorialLength(1000) shouldBe 2568
+  }
+
+  "#factorialLength" should "return 35660 for 10000!" in {
+    Recursion.factorialLength(10000) shouldBe 35660
+  }
+
+  "#maxSubsequence" should "return 6 for {1,2,3}" in {
+    Recursion.maxSubsequence(List(1,2,3)) shouldBe 6
+  }
+
+  "#maxSubsequence" should "return 2 for {1,1,-1}" in {
+    Recursion.maxSubsequence(List(1,1,-1)) shouldBe 2
+  }
+
+  "#maxSubsequence" should "return 214 for {-1,7,114,93,-15,3}" in {
+    Recursion.maxSubsequence(List(-1,7,114,93,-15,3)) shouldBe 214
+  }
+
+  "#maxSubsequence" should "return 1 for {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}" in {
+    Recursion.maxSubsequence(List(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1)) shouldBe 1
+  }
+
+  "#maxSubsequence" should "return 1234567890 for {1234567890,-123456789}" in {
+    Recursion.maxSubsequence(List(1234567890,-123456789)) shouldBe 1234567890
+  }
 }
